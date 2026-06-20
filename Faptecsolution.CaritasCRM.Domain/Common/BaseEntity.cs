a6a7b046caa6c60ae94b0c0d633e2b8501cd42d2
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-20 23:52:35 +0300

    Add BaseEntity abstract class with common entity fields
    Introduced BaseEntity.cs in the Faptecsolution.CaritasCRM.Domain.Common namespace. This abstract class provides standard properties for entities, including Id, CreatedOn, CreatedBy, ModifiedOn, ModifiedBy, IsDeleted, and RowVersion, with default values for key fields.

---
+++ Faptecsolution.CaritasCRM.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Faptecsolution.CaritasCRM.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public Guid? CreatedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public Guid? ModifiedBy { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public byte[]? RowVersion { get; set; }
+    }
+
+}
