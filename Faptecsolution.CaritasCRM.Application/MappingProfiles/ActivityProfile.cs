0bf046dab6bfad4d99d1cda9858521a3c9286459
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-23 00:30:33 +0300

    Add ActivityProfile for mapping Activity to DTO
    Introduced ActivityProfile using AutoMapper to map Activity entities to ActivitySummaryDTO, converting enum properties (ActivityType, Priority, Status) to their string values.

---
+++ Faptecsolution.CaritasCRM.Application/MappingProfiles/ActivityProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetLeadsDetail;
+using Faptecsolution.CaritasCRM.Domain.Entities;
+
+namespace Faptecsolution.CaritasCRM.Application.MappingProfiles
+{
+    public class ActivityProfile : Profile
+    {
+        public ActivityProfile()
+        {
+            CreateMap<Activity, ActivitySummaryDTO>()
+                .ForMember(d => d.ActivityType, o => o.MapFrom(s => s.ActivityType.ToString()))
+                .ForMember(d => d.Priority, o => o.MapFrom(s => s.Priority.ToString()))
+                .ForMember(d => d.Status, o => o.MapFrom(s => s.Status.ToString()));
+        }
+    }
+}
