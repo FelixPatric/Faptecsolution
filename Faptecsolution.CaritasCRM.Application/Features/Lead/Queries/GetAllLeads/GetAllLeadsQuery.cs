0152704aea4e87b0e7d88e54aa5dd6e2faa8834f
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 13:31:37 +0300

    Add GetAllLeadsQuery record for lead retrieval
    Introduced GetAllLeadsQuery in a new namespace to support MediatR-based retrieval of all leads as LeadDTO objects. Added required MediatR using directive.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Queries/GetAllLeads/GetAllLeadsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetAllLeads
+{
+    public record GetAllLeadsQuery : IRequest<List<LeadDTO>>
+    {
+    }
+}
