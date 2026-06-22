88360c7e3cff381a0fc8e6bbc4f670979ca828cc
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 13:33:03 +0300

    Add GetAllLeadsQueryHandler for retrieving all leads
    Implemented GetAllLeadsQueryHandler using MediatR to handle GetAllLeadsQuery requests. The handler retrieves all leads from the repository, maps them to LeadDTOs with AutoMapper, and returns the result. Dependency injection is used for repository and mapper.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Queries/GetAllLeads/GetAllLeadsQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetAllLeads
+{
+    public class GetAllLeadsQueryHandler : IRequestHandler<GetAllLeadsQuery, List<LeadDTO>>
+    {
+        private readonly ILeadRepository _leadRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllLeadsQueryHandler(ILeadRepository leadRepository, IMapper mapper)
+        {
+            _leadRepository = leadRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<LeadDTO>> Handle(GetAllLeadsQuery request, CancellationToken cancellationToken)
+        {
+            // 1. Query the database
+            var leads = await _leadRepository.GetAsync();
+
+            // 2. Convert data objects to DTOs objects
+            var data = _mapper.Map<List<LeadDTO>>(leads);
+
+            // 3. Return the list of DTOs
+            return data;
+        }
+    }
+}
