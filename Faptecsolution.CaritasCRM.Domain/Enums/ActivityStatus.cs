878221276618a9eb67f87a1addfedb87b90f2630
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 14:02:53 +0300

    Add ActivityStatus enum to domain enums
    Introduced the ActivityStatus enum in the Faptecsolution.CaritasCRM.Domain.Enums namespace with values: Scheduled, InProgress, Completed, Canceled, and Deferred. This will help standardize activity status representation across the application.

---
+++ Faptecsolution.CaritasCRM.Domain/Enums/ActivityStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace Faptecsolution.CaritasCRM.Domain.Enums
+{
+    public enum ActivityStatus
+    {
+        Scheduled,
+        InProgress,
+        Completed,
+        Canceled,
+        Deferred
+
+    }
+
+
+}
+
