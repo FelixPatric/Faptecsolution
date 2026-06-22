25ee664c811b0016a1e3c0d5ba3cceb3c4858623
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 20:35:22 +0300

    Add GetLeadDetailsQuery for lead detail retrieval
    Introduced GetLeadDetailsQuery class implementing MediatR's IRequest for LeadDetailsDTO, enabling retrieval of lead details by Id.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Queries/GetLeadsDetail/GetLeadDetailsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetLeadsDetail
+{
+    public class GetLeadDetailsQuery : IRequest<LeadDetailsDTO>
+    {
+        public Guid Id { get; set; }
+    }
+}
