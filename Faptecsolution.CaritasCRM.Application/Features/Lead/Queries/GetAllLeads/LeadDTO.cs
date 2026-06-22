e6be57c48755e195cfef41b6902b779eccb90c18
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 13:14:04 +0300

    Add LeadDTO class for lead query feature
    Introduced LeadDTO.cs with properties for lead details such as Id, Topic, FirstName, LastName, Company, Status, Rating, and CreatedOn in the appropriate namespace to support lead query operations.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Queries/GetAllLeads/LeadDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetAllLeads
+{
+    public class LeadDTO
+    {
+        public Guid Id { get; set; }
+        public string Topic { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Company { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Rating { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; }
+    }
+
+}
