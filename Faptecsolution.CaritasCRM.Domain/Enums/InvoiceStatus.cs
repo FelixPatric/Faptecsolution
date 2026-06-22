ba4c9cdbe1e32e971ce6015dd5ca22a6e7702670
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 14:03:32 +0300

    Add InvoiceStatus enum to represent invoice states
    Introduced the InvoiceStatus enum in the Faptecsolution.CaritasCRM.Domain.Enums namespace. This enum defines possible invoice states such as Draft, Sent, Partial, Paid, Overdue, Canceled, and Refunded, each with descriptive comments for clarity.

---
+++ Faptecsolution.CaritasCRM.Domain/Enums/InvoiceStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace Faptecsolution.CaritasCRM.Domain.Enums
+{
+    public enum InvoiceStatus
+    {
+        Draft,          // Just created, not sent
+        Sent,           // Sent to customer
+        Partial,        // Partially paid
+        Paid,           // Fully paid
+        Overdue,        // Past due date
+        Canceled,       // Canceled
+        Refunded        // Refunded to customer
+    }
+
+
+}
+
