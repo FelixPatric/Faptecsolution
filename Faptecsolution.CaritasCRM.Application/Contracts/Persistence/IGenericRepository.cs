23625406371a4e071305e13954b4bde5e270884f
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 20:16:15 +0300

    Update GetByIdAsync to use Guid instead of int
    Changed the GetByIdAsync method in IGenericRepository to accept a Guid parameter, standardizing entity identification to use Guids rather than integers.

--- Faptecsolution.CaritasCRM.Application/Contracts/Persistence/IGenericRepository.cs
+++ Faptecsolution.CaritasCRM.Application/Contracts/Persistence/IGenericRepository.cs
@@ -5,7 +5,7 @@ namespace Faptecsolution.CaritasCRM.Application.Contracts.Persistence
     public interface IGenericRepository<T> where T : BaseEntity
     {
         Task<IReadOnlyList<T>> GetAsync();
-        Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(Guid id);
         Task CreateAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
