524a8dd058bd827b334c5e23a9d506887a2e036b
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 12:55:01 +0300

    Add ApplicationServiceRegistration for DI setup
    Introduced ApplicationServiceRegistration with an AddApplicationServices extension method to register AutoMapper and MediatR for dependency injection in the application layer.

---
+++ Faptecsolution.CaritasCRM.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Faptecsolution.CaritasCRM.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Register application services here
+            services.AddAutoMapper(cfg => { }, typeof(ApplicationServiceRegistration).Assembly);
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ApplicationServiceRegistration).Assembly));  
+            return services;
+        }
+    }
+}
