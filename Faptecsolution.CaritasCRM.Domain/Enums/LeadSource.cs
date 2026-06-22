559270740cf09d277225074292994b829174d64e
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 14:03:59 +0300

    Add LeadSource enum to define lead origin types
    Introduced a new LeadSource enum in the Faptecsolution.CaritasCRM.Domain.Enums namespace to represent various sources of leads, including Advertisement, EmployeeReferral, ExternalReferral, Partner, PublicRelations, Seminar, TradeShow, Web, WordOfMouth, and Other. This will help standardize lead source tracking across the application.

---
+++ Faptecsolution.CaritasCRM.Domain/Enums/LeadSource.cs
@@ -0,0 +1,20 @@
+﻿namespace Faptecsolution.CaritasCRM.Domain.Enums
+{
+    public enum LeadSource
+    {
+        Advertisement,
+        EmployeeReferral,
+        ExternalReferral,
+        Partner,
+        PublicRelations,
+        Seminar,
+        TradeShow,
+        Web,
+        WordOfMouth,
+        Other
+
+    }
+
+
+}
+
