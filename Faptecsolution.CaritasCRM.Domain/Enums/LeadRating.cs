b147736bf3fd34767291653f938ddfaa532ea810
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 14:03:44 +0300

    Add LeadRating enum to represent lead probability levels
    Introduced the LeadRating enum in the Faptecsolution.CaritasCRM.Domain.Enums namespace with values Hot, Warm, and Cold to categorize leads by probability and urgency.

---
+++ Faptecsolution.CaritasCRM.Domain/Enums/LeadRating.cs
@@ -0,0 +1,12 @@
+﻿namespace Faptecsolution.CaritasCRM.Domain.Enums
+{
+    public enum LeadRating
+    {
+        Hot,  // High probability, immediate need
+        Warm, // Medium probability, future need
+        Cold  // Low probability, no immediate need    
+    }
+
+
+}
+
