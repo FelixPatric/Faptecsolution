8cd8b7b2ab171d3b2a9bbadbf6dc418ffb75b2a2
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-22 13:29:44 +0300

    Add LeadProfile for AutoMapper LeadDTO <-> Lead mapping
    Introduced LeadProfile class in the MappingProfiles namespace to enable bidirectional mapping between LeadDTO and Lead entities using AutoMapper. Added required using directives for AutoMapper, LeadDTO, and Lead.

---
+++ Faptecsolution.CaritasCRM.Application/MappingProfiles/LeadProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetAllLeads;
+using Faptecsolution.CaritasCRM.Domain.Entities;
+
+namespace Faptecsolution.CaritasCRM.Application.MappingProfiles
+{
+    public class LeadProfile : Profile
+    {
+        public LeadProfile()
+        {
+            CreateMap<LeadDTO, Lead>().ReverseMap();
+        }
+    }
+}
