120d8f204333b6fb657efbd60af03285b6494dbb
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-24 22:45:29 +0300

    Remove validation from CreateLeadCommandHandler
    Removed all validation logic from CreateLeadCommandHandler, including the use of CreateLeadCommandValidator and BadRequestException. The handler now maps and saves the lead without input validation.

--- Faptecsolution.CaritasCRM.Application/Features/Lead/Commands/CreateLeadCommand/CreateLeadCommandHandler.cs
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Commands/CreateLeadCommand/CreateLeadCommandHandler.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
-using Faptecsolution.CaritasCRM.Application.Exceptions;
 using MediatR;
 
 namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.CreateLeadCommand
@@ -18,23 +17,10 @@ namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.CreateLea
 
         public async Task<Guid> Handle(CreateLeadCommand request, CancellationToken cancellationToken)
         {
-            // 1. Validate the incoming data 
-            var validator = new CreateLeadCommandValidator(_leadRepository);
-
-            var validationResult = await validator.ValidateAsync(request);
-            if (validationResult.Errors.Any())
-            {
-                throw new BadRequestException("Invalid lead data.", validationResult);
-            }
-
-            // 2. Convert to domain entity object
             var leadToCreate = _mapper.Map<Domain.Entities.Lead>(request);
 
-            // 3. Add to database
             await _leadRepository.CreateAsync(leadToCreate);
 
-
-            // 4. Return the ID of the newly created lead
             return leadToCreate.Id;
         }
     }
