057e414d35d9f6ac563021f5cc7822e09c2ee8b7
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-24 19:27:07 +0300

    Add SearchLeadsQuery for lead search functionality
    Introduced SearchLeadsQuery record in the SearchLeads namespace. This enables searching leads by a search term using MediatR, returning a list of LeadDTOs.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Queries/SearchLeads/SearchLeadsQuery.cs
@@ -0,0 +1,7 @@
+﻿using Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetAllLeads;
+using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.SearchLeads
+{
+    public record SearchLeadsQuery(string SearchTerm) : IRequest<List<LeadDTO>>;
+}
