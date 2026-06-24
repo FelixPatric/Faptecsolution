6ec809f5133bfba07083aaf220024a397c60993d
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-24 20:00:24 +0300

    Add ValidationResult support to BadRequestException
    Added a constructor to BadRequestException that accepts a ValidationResult and populates a new ValidationErrors property with error messages. Imported FluentValidation.Results to enable this functionality.

--- Faptecsolution.CaritasCRM.Application/Exceptions/BadRequestException.cs
+++ Faptecsolution.CaritasCRM.Application/Exceptions/BadRequestException.cs
@@ -1,9 +1,21 @@
-﻿namespace Faptecsolution.CaritasCRM.Application.Exceptions
+﻿using FluentValidation.Results;
+
+namespace Faptecsolution.CaritasCRM.Application.Exceptions
 {
     public class BadRequestException : Exception
     {
         public BadRequestException(string message) : base(message)
         {
         }
+        public BadRequestException(string message, ValidationResult validationResult) : base(message)
+        {
+            ValidationErrors = new();
+            foreach (var error in validationResult.Errors)
+            {
+                ValidationErrors.Add(error.ErrorMessage);
+            }
+        }
+
+        public List<string> ValidationErrors { get; set; }
     }
 }
