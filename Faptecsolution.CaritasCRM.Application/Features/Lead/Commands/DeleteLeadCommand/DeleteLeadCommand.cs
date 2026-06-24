527c4276877c26dcc656a44a3c6e19da0ee99b23
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-24 15:00:46 +0300

    Add DeleteLeadCommand for lead deletion via MediatR
    Introduced DeleteLeadCommand class implementing IRequest<Unit> to handle lead deletion requests by Id in the application layer.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Commands/DeleteLeadCommand/DeleteLeadCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.DeleteLeadCommand
+{
+    public class DeleteLeadCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+    }
+}
