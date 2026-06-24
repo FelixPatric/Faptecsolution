45e1d40c4de21975124be854d46f8090221e2a78
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-24 15:00:57 +0300

    Add DeleteLeadCommandHandler for soft-deleting leads
    Implemented DeleteLeadCommandHandler using MediatR to handle lead deletion requests. The handler marks the lead as deleted by setting IsDeleted to true and calls DeleteAsync. NotFoundException logic is present but commented out.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Commands/DeleteLeadCommand/DeleteLeadCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.DeleteLeadCommand
+{
+    public class DeleteLeadCommandHandler : IRequestHandler<DeleteLeadCommand, Unit>
+    {
+        private readonly ILeadRepository _leadRepository;
+
+        public DeleteLeadCommandHandler(ILeadRepository leadRepository)
+        {
+            _leadRepository = leadRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteLeadCommand request, CancellationToken cancellationToken)
+        {
+            var leadToDelete = await _leadRepository.GetByIdAsync(request.Id);
+
+            // Check if the lead exists
+            //if (leadToDelete == null)
+            //{
+            //    throw new NotFoundException(nameof(Lead), request.Id);
+            //}
+            leadToDelete.IsDeleted = true;
+            await _leadRepository.DeleteAsync(leadToDelete);
+
+            return Unit.Value;
+
+        }
+    }
+}
