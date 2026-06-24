10a06f483b2f6535a2b7c6dd5517f9e2edbaebb3
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-24 19:27:56 +0300

    Add SearchLeadsQueryValidator with FluentValidation rules
    Introduced SearchLeadsQueryValidator to validate the SearchTerm property in SearchLeadsQuery. Enforces required, minimum length of 2, and maximum length of 100 characters using FluentValidation.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Queries/SearchLeads/SearchLeadsQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.SearchLeads
+{
+    public class SearchLeadsQueryValidator : AbstractValidator<SearchLeadsQuery>
+    {
+        public SearchLeadsQueryValidator()
+        {
+            RuleFor(x => x.SearchTerm)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .MinimumLength(2).WithMessage("{PropertyName} must be at least 2 characters.")
+                .MaximumLength(100).WithMessage("{PropertyName} cannot exceed 100 characters.");
+        }
+    }
+}
