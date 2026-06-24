e250d7d3fdde3d18530493abf30d546b2058d8b6
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-24 15:19:06 +0300

    Add NotFoundException for entity not found scenarios
    Introduced NotFoundException in the Faptecsolution.CaritasCRM.Application.Exceptions namespace. This custom exception provides a clear error message when a specified entity is not found, improving error handling and debugging.

---
+++ Faptecsolution.CaritasCRM.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Faptecsolution.CaritasCRM.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key) : base($"{name} ({key}) was not found.")
+        {
+        }
+    }
+}
