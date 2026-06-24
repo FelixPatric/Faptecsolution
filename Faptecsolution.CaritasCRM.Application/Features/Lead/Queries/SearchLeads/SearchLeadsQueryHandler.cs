375d95bbb69a65244e991770c90cbc7be4c4640b
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-24 19:46:46 +0300

    Add SearchLeadsQueryHandler for lead search functionality
    Introduced SearchLeadsQueryHandler using MediatR to handle lead searches via ILeadRepository. Utilized AutoMapper for mapping results to LeadDTO. Added required dependencies and using directives.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Queries/SearchLeads/SearchLeadsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetAllLeads;
+using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.SearchLeads
+{
+    public class SearchLeadsQueryHandler : IRequestHandler<SearchLeadsQuery, List<LeadDTO>>
+    {
+        private readonly ILeadRepository _leadRepository;
+        private readonly IMapper _mapper;
+
+        public SearchLeadsQueryHandler(ILeadRepository leadRepository, IMapper mapper)
+        {
+            _leadRepository = leadRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<LeadDTO>> Handle(SearchLeadsQuery request, CancellationToken cancellationToken)
+        {
+            var leads = await _leadRepository.SearchAsync(request.SearchTerm);
+
+            return _mapper.Map<List<LeadDTO>>(leads);
+        }
+    }
+}
