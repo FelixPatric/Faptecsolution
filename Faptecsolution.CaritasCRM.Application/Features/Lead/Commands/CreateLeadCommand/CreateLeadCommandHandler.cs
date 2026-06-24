9bcbaf815d01dd70b0c103a029dc3001714beb7d
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-24 14:11:44 +0300

    Add CreateLeadCommandHandler for lead creation
    Introduced CreateLeadCommandHandler to handle CreateLeadCommand requests using MediatR. Utilizes AutoMapper for mapping and ILeadRepository for persisting new leads, returning the created lead's ID.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Commands/CreateLeadCommand/CreateLeadCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.CreateLeadCommand
+{
+    public class CreateLeadCommandHandler : IRequestHandler<CreateLeadCommand, Guid>
+    {
+        private readonly ILeadRepository _leadRepository;
+        private readonly IMapper _mapper;
+
+        public CreateLeadCommandHandler(ILeadRepository leadRepository, IMapper mapper)
+        {
+            _leadRepository = leadRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Guid> Handle(CreateLeadCommand request, CancellationToken cancellationToken)
+        {
+            // 1. Validate the incoming data 
+
+            // 2. Convert to domain entity object
+            var leadToCreate = _mapper.Map<Domain.Entities.Lead>(request);
+
+            // 3. Add to database
+            await _leadRepository.CreateAsync(leadToCreate);
+
+
+            // 4. Return the ID of the newly created lead
+            return leadToCreate.Id;
+        }
+    }
+}
