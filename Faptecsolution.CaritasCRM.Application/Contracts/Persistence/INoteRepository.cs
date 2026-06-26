af215a085fba1a2196b6b6b84ffdd5db0baef6ea
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-26 21:09:57 +0300

    Add INoteRepository interface for note retrieval methods
    Introduced INoteRepository extending IGenericRepository<Note> with methods to fetch notes by regarding object and to get recent notes, supporting filtering by object ID, type, and limiting results.

---
+++ Faptecsolution.CaritasCRM.Application/Contracts/Persistence/INoteRepository.cs
@@ -0,0 +1,11 @@
+﻿using Faptecsolution.CaritasCRM.Domain.Entities;
+
+namespace Faptecsolution.CaritasCRM.Application.Contracts.Persistence
+{
+    public interface INoteRepository : IGenericRepository<Note>
+    {
+        Task<IReadOnlyList<Note>> GetByRegardingAsync(Guid regardingObjectId, string regardingObjectType);
+
+        Task<IReadOnlyList<Note>> GetRecentByRegardingAsync(Guid regardingObjectId, string regardingObjectType, int take = 10);
+    }
+}
