5e67928f9d34908e36caf4fdd6f1a8dcf9fa4738
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 12:54:28 +0300

    Add NoteProfile for mapping AddNoteCommand to Note
    Introduced NoteProfile in the MappingProfiles namespace to map AddNoteCommand to the Note entity using AutoMapper. Added required using directives.

---
+++ Faptecsolution.CaritasCRM.Application/MappingProfiles/NoteProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Faptecsolution.CaritasCRM.Application.Features.Notes.Commands.AddNoteCommand;
+using Faptecsolution.CaritasCRM.Domain.Entities;
+
+namespace Faptecsolution.CaritasCRM.Application.MappingProfiles
+{
+    public class NoteProfile : Profile
+    {
+        public NoteProfile()
+        {
+            CreateMap<AddNoteCommand, Note>();
+        }
+    }
+}
