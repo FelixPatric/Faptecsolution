0a9c8ee32bf8f7b62d0b00b8b4384f9f2b9b99e5
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 15:00:56 +0300

    Add PersistenceServiceRegistration for DbContext DI setup
    Introduced PersistenceServiceRegistration with an AddPersistenceServices extension method to register CaritasCRMDbContext using SQL Server and configuration-based connection string. Added required using directives for EF Core and DI.

---
+++ Faptecsolution.CaritasCRM.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Faptecsolution.CaritasCRM.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Faptecsolution.CaritasCRM.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Register your persistence services here
+            services.AddDbContext<CaritasCRMDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CaritasCRMConnectionString"));
+            });
+            return services;
+        }
+    }
+}
