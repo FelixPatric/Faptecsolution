31b36cb910fa373c11ea0c2bcd2bff431f2f89b7
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 18:14:43 +0300

    Add OpportunityRepository implementation
    Created OpportunityRepository class in the Persistence layer, implementing IOpportunityRepository and extending GenericRepository<Opportunity>. Utilizes dependency injection for CaritasCRMDbContext.

---
+++ Faptecsolution.CaritasCRM.Persistence/Repositories/OpportunityRepository.cs
@@ -0,0 +1,15 @@
+﻿using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using Faptecsolution.CaritasCRM.Domain.Entities;
+using Faptecsolution.CaritasCRM.Persistence.DatabaseContext;
+
+namespace Faptecsolution.CaritasCRM.Persistence.Repositories
+{
+    public class OpportunityRepository : GenericRepository<Opportunity>, IOpportunityRepository
+    {
+        public OpportunityRepository(CaritasCRMDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+
+
+}
