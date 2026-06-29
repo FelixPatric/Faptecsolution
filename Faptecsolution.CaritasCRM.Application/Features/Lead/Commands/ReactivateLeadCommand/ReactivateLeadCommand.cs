342f620c16108bcd9745a5d450fd4abdb99737c4
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 08:58:48 +0300

    Add ReactivateLeadCommand for lead reactivation requests
    Introduced ReactivateLeadCommand class implementing MediatR's IRequest<Unit> in a new file. This command includes LeadId and an optional ReactivationNote to support lead reactivation functionality.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Commands/ReactivateLeadCommand/ReactivateLeadCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.ReactivatLeadCommand
+{
+    public class ReactivateLeadCommand : IRequest<Unit>
+    {
+        public Guid LeadId { get; set; }
+        public string? ReactivationNote { get; set; }
+    }
+}
