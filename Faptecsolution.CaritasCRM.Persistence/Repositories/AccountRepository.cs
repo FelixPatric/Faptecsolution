3f4b18e2da1e5c3bdf1d5eae06a8c22fbcac68ed
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 18:12:36 +0300

    Add AccountRepository implementation
    Created AccountRepository class inheriting from GenericRepository<Account> and implementing IAccountRepository. Added constructor with CaritasCRMDbContext dependency and necessary using directives.

---
+++ Faptecsolution.CaritasCRM.Persistence/Repositories/AccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using Faptecsolution.CaritasCRM.Domain.Entities;
+using Faptecsolution.CaritasCRM.Persistence.DatabaseContext;
+
+namespace Faptecsolution.CaritasCRM.Persistence.Repositories
+{
+    public class AccountRepository : GenericRepository<Account>, IAccountRepository
+    {
+        public AccountRepository(CaritasCRMDbContext dbContext) : base(dbContext)
+        {
+        }
+
+    }
+
+
+}
