4082b68565eea73aab4dd509873b043b40dcd616
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 18:14:31 +0300

    Add NoteRepository implementation
    Introduced NoteRepository in the Persistence layer, inheriting from GenericRepository<Note> and implementing INoteRepository. Added required constructor and using directives.

---
+++ Faptecsolution.CaritasCRM.Persistence/Repositories/NoteRepository.cs
@@ -0,0 +1,15 @@
+﻿using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using Faptecsolution.CaritasCRM.Domain.Entities;
+using Faptecsolution.CaritasCRM.Persistence.DatabaseContext;
+
+namespace Faptecsolution.CaritasCRM.Persistence.Repositories
+{
+    public class NoteRepository : GenericRepository<Note>, INoteRepository
+    {
+        public NoteRepository(CaritasCRMDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+
+
+}
