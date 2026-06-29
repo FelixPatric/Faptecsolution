394b5a0152f8b8ac9e41e05089d5394ca5c05535
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 08:43:48 +0300

    Add DisqualifyLeadCommandValidator with validation rules
    Introduced DisqualifyLeadCommandValidator using FluentValidation to ensure LeadId and Reason are required fields, and Reason does not exceed 250 characters.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Commands/DisqualifyLeadCommand/DisqualifyLeadCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.DisqualifyLeadCommand
+{
+    public class DisqualifyLeadCommandValidator : AbstractValidator<DisqualifyLeadCommand>
+    {
+        public DisqualifyLeadCommandValidator()
+        {
+            RuleFor(x => x.LeadId)
+                .NotEmpty().WithMessage("{PropertyName} is required.");
+
+            RuleFor(x => x.Reason)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .MaximumLength(250).WithMessage("{PropertyName} cannot exceed 250 characters.");
+        }
+    }
+}
