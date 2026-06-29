7b22f9da7d2981561760cfcbf394ca7e774555f6
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 18:13:04 +0300

    Add ActivityRepository implementation
    Created ActivityRepository class inheriting from GenericRepository<Activity> and implementing IActivityRepository. Added constructor with CaritasCRMDbContext dependency and necessary using directives.

---
+++ Faptecsolution.CaritasCRM.Persistence/Repositories/ActivityRepository.cs
@@ -0,0 +1,15 @@
+﻿using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using Faptecsolution.CaritasCRM.Domain.Entities;
+using Faptecsolution.CaritasCRM.Persistence.DatabaseContext;
+
+namespace Faptecsolution.CaritasCRM.Persistence.Repositories
+{
+    public class ActivityRepository : GenericRepository<Activity>, IActivityRepository
+    {
+        public ActivityRepository(CaritasCRMDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+
+
+}
