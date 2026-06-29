f97aa636e1a3593dbc5d8441e6d1904285a66a50
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 18:14:54 +0300

    Add ProductRepository implementation
    Created ProductRepository class inheriting from GenericRepository<Product> and implementing IProductRepository. Added constructor with CaritasCRMDbContext dependency and necessary using directives.

---
+++ Faptecsolution.CaritasCRM.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using Faptecsolution.CaritasCRM.Domain.Entities;
+using Faptecsolution.CaritasCRM.Persistence.DatabaseContext;
+
+namespace Faptecsolution.CaritasCRM.Persistence.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(CaritasCRMDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+
+
+}
