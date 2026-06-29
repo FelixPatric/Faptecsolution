bf6f382b08905fb87cc514e9f9f936f6519315f6
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 12:26:27 +0300

    Update ActivityProfile to map Activity to TimelineItemDTO
    Changed mapping in ActivityProfile from ActivitySummaryDTO to TimelineItemDTO. Retained custom member mappings for ActivityType, Priority, and Status properties.

--- Faptecsolution.CaritasCRM.Application/MappingProfiles/ActivityProfile.cs
+++ Faptecsolution.CaritasCRM.Application/MappingProfiles/ActivityProfile.cs
@@ -8,7 +8,7 @@ namespace Faptecsolution.CaritasCRM.Application.MappingProfiles
     {
         public ActivityProfile()
         {
-            CreateMap<Activity, ActivitySummaryDTO>()
+            CreateMap<Activity, TimelineItemDTO>()
                 .ForMember(d => d.ActivityType, o => o.MapFrom(s => s.ActivityType.ToString()))
                 .ForMember(d => d.Priority, o => o.MapFrom(s => s.Priority.ToString()))
                 .ForMember(d => d.Status, o => o.MapFrom(s => s.Status.ToString()));
