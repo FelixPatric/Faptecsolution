4576bf8299d172f5f248c84d199b5e5c2e0c6130
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 08:44:37 +0300

    Add DisqualifyLeadCommand for lead disqualification
    Introduced DisqualifyLeadCommand class implementing IRequest<Unit> with LeadId and Reason properties to support lead disqualification functionality.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Commands/DisqualifyLeadCommand/DisqualifyLeadCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.DisqualifyLeadCommand
+{
+    public class DisqualifyLeadCommand : IRequest<Unit>
+    {
+        public Guid LeadId { get; set; }
+        public string Reason { get; set; } = string.Empty;
+    }
+}
