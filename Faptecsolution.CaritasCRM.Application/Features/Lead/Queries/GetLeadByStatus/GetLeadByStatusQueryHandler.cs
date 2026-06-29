4272cfc77984abe71998b346cf697061eb017f88
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 12:24:38 +0300

    Add GetLeadByStatusQueryHandler for lead status queries
    Implemented GetLeadByStatusQueryHandler to handle GetLeadByStatusQuery requests using MediatR. The handler retrieves leads by status from ILeadRepository and maps them to LeadDTOs using AutoMapper.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Queries/GetLeadByStatus/GetLeadByStatusQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetAllLeads;
+using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetLeadByStatus
+{
+    public class GetLeadByStatusQueryHandler : IRequestHandler<GetLeadByStatusQuery, IEnumerable<LeadDTO>>
+    {
+        private readonly ILeadRepository _leadRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeadByStatusQueryHandler(ILeadRepository leadRepository, IMapper mapper)
+        {
+            _leadRepository = leadRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<LeadDTO>> Handle(GetLeadByStatusQuery request, CancellationToken cancellationToken)
+        {
+            var leadsByStatus = await _leadRepository.GetLeadsByStatusAsync(request.leadStatus);
+
+            return _mapper.Map<IEnumerable<LeadDTO>>(leadsByStatus);
+        }
+    }
+}
