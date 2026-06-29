ffd79eb306d2dddec464bf1832a6414b5a8b3b2b
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 12:24:24 +0300

    Add GetLeadByStatusQuery for filtering leads by status
    Introduced GetLeadByStatusQuery in the Lead.Queries namespace to enable retrieving LeadDTO collections filtered by LeadStatus using MediatR. Added required using directives.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Queries/GetLeadByStatus/GetLeadByStatusQuery.cs
@@ -0,0 +1,10 @@
+﻿using Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetAllLeads;
+using Faptecsolution.CaritasCRM.Domain.Enums;
+using MediatR;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetLeadByStatus
+{
+    public record GetLeadByStatusQuery(LeadStatus leadStatus) : IRequest<IEnumerable<LeadDTO>>
+    {
+    }
+}
