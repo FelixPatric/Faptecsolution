9f6d3fd1c356331be076fb418f4f36e6c9dc4590
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 08:35:12 +0300

    Add DisqualifyReason enum for lead disqualification reasons
    Introduced the DisqualifyReason enum in the Faptecsolution.CaritasCRM.Domain.Enums namespace to standardize reasons for lead disqualification, including options like CannotContact, NoLongerInterested, Canceled, Lost, and NotInTargetAudience.

---
+++ Faptecsolution.CaritasCRM.Domain/Enums/DisqualifyReason.cs
@@ -0,0 +1,11 @@
+﻿namespace Faptecsolution.CaritasCRM.Domain.Enums
+{
+    public enum DisqualifyReason
+    {
+        CannotContact,
+        NoLongerInterested,
+        Canceled,
+        Lost,
+        NotInTargetAudience
+    }
+}
