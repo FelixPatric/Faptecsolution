24e63cb5aec4c393f676db87b14c503d406921d6
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 12:35:19 +0300

    Add mappings for Lead create and update commands
    Added AutoMapper profiles for CreateLeadCommand and UpdateLeadCommand to Lead entity. UpdateLeadCommand mapping ignores system and qualified fields to prevent unintended overwrites. Updated using statements for new command classes.

--- Faptecsolution.CaritasCRM.Application/MappingProfiles/LeadProfile.cs
+++ Faptecsolution.CaritasCRM.Application/MappingProfiles/LeadProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.CreateLeadCommand;
+using Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.UpdateLeadCommand;
 using Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetAllLeads;
 using Faptecsolution.CaritasCRM.Application.Features.Lead.Queries.GetLeadsDetail;
 using Faptecsolution.CaritasCRM.Domain.Entities;
@@ -16,6 +18,20 @@ namespace Faptecsolution.CaritasCRM.Application.MappingProfiles
                 .ForMember(d => d.Source, o => o.MapFrom(s => s.Source.ToString()))
                 .ForMember(d => d.Rating, o => o.MapFrom(s => s.Rating.ToString()))
                 .ForMember(d => d.Timeline, o => o.Ignore());
+
+            CreateMap<CreateLeadCommand, Lead>();
+
+            CreateMap<UpdateLeadCommand, Lead>()
+                .ForMember(d => d.Id, o => o.Ignore())
+                .ForMember(d => d.CreatedOn, o => o.Ignore())
+                .ForMember(d => d.CreatedBy, o => o.Ignore())
+                .ForMember(d => d.ModifiedOn, o => o.Ignore())
+                .ForMember(d => d.ModifiedBy, o => o.Ignore())
+                .ForMember(d => d.IsDeleted, o => o.Ignore())
+                .ForMember(d => d.RowVersion, o => o.Ignore())
+                .ForMember(d => d.QualifiedContact, o => o.Ignore())
+                .ForMember(d => d.QualifiedAccount, o => o.Ignore())
+                .ForMember(d => d.QualifiedOpportunity, o => o.Ignore());
         }
     }
 }
