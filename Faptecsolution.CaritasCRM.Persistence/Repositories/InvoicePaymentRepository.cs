728307044c9073e880dcb75478ed0916bce2cb61
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 18:13:41 +0300

    Add InvoicePaymentRepository implementation
    Created InvoicePaymentRepository class inheriting from GenericRepository and implementing IInvoicePaymentRepository. Added necessary constructor and using directives.

---
+++ Faptecsolution.CaritasCRM.Persistence/Repositories/InvoicePaymentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Faptecsolution.CaritasCRM.Application.Contracts.Persistence;
+using Faptecsolution.CaritasCRM.Domain.Entities;
+using Faptecsolution.CaritasCRM.Persistence.DatabaseContext;
+
+namespace Faptecsolution.CaritasCRM.Persistence.Repositories
+{
+    public class InvoicePaymentRepository : GenericRepository<InvoicePayment>, IInvoicePaymentRepository
+    {
+        public InvoicePaymentRepository(CaritasCRMDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+
+
+}
