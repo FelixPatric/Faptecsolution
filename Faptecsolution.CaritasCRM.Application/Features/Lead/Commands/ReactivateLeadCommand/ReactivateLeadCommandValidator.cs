33853ab4ae5e2252d5ba635ea6d73e7e2d20c48e
Felix Patric <felixpatricadhiambo@hotmail.com>
2026-06-29 09:04:19 +0300

    Add ReactivateLeadCommandValidator with validation rules
    Added ReactivateLeadCommandValidator.cs using FluentValidation.
    Ensures LeadId is required and ReactivationNote is at most 250
    characters if provided.

---
+++ Faptecsolution.CaritasCRM.Application/Features/Lead/Commands/ReactivateLeadCommand/ReactivateLeadCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Faptecsolution.CaritasCRM.Application.Features.Lead.Commands.ReactivateLeadCommand
+{
+    public class ReactivateLeadCommandValidator : AbstractValidator<ReactivateLeadCommand>
+    {
+        public ReactivateLeadCommandValidator()
+        {
+            RuleFor(x => x.LeadId)
+                .NotEmpty().WithMessage("{PropertyName} is required.");
+
+            RuleFor(x => x.ReactivationNote)
+                .MaximumLength(250).WithMessage("{PropertyName} cannot exceed 250 characters.")
+                .When(x => !string.IsNullOrEmpty(x.ReactivationNote));
+        }
+    }
+}
